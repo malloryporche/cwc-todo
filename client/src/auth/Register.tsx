1cf4e342cea45d02f063c6738f813642202b8a6f
malloryporche <malloryburke@gmail.com>
2024-02-06 17:08:53 -0500

    Fixed Registration form

--- client/src/auth/Register.tsx
+++ client/src/auth/Register.tsx
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import {
+  Container,
   Input,
   InputGroup,
   InputRightElement,
   Button,
-  Stack,
+  FormControl,
+  FormLabel,
 } from "@chakra-ui/react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons"; //ViewIcon, ViewOffIcon
 import axios from "axios";
+import { Form } from "react-router-dom";
 
 export interface User {
   name: string;
   email: string;
-  password: string;
+  pass: string;
   darkMode: boolean;
 }
 
@@ -20,7 +23,7 @@ export default function Register() {
   const [user, setUser] = useState({
     name: "",
     email: "",
-    password: "",
+    pass: "",
     darkMode: true,
   });
   const [show, setShow] = useState(false);
@@ -28,11 +31,17 @@ export default function Register() {
 
   const registerUser = (e: React.FormEvent, user: User) => {
     e.preventDefault();
-    console.log(user);
+
     axios
-      .post("/auth/register", user)
+      .post("http://localhost:3001/auth/register", user)
       .then((res) => {
         console.log(res.data);
+        setUser({
+          name: "",
+          email: "",
+          pass: "",
+          darkMode: true,
+        });
         //redirect to user dashboard
       })
       .catch((err) => {
@@ -40,37 +49,59 @@ export default function Register() {
       });
   };
   return (
-    <div>
+    <Container>
       <h1>Register</h1>
-      <Stack spacing={3}>
-        <Input
-          type="text"
-          color="blue"
-          placeholder="Enter your name"
-          onChange={(e) => setUser({ ...user, name: e.target.value })}
-        />
-        <Input
-          type="email"
-          color="blue"
-          placeholder="Enter your email"
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-        />
-        <InputGroup size="md">
+      <Form onSubmit={(e) => registerUser(e, user)}>
+        <FormControl isRequired>
+          <FormLabel htmlFor="username">Name</FormLabel>
           <Input
-            type={show ? "text" : "password"}
+            placeholder="Enter your name"
             color="blue"
-            placeholder="Enter a password."
+            type="text"
+            name="username"
+            id="username"
+            autoComplete="name"
+            value={user.name}
+            onChange={(e) => setUser({ ...user, name: e.target.value })}
           />
-          <InputRightElement width="4.5rem">
-            <Button h="1.75rem" size="sm" onClick={handleClick}>
-              {show ? <ViewOffIcon /> : <ViewIcon />}
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-        <Button colorScheme="blue" onSubmit={(e) => registerUser(e, user)}>
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <Input
+            type="email"
+            placeholder="Enter your email"
+            color="blue"
+            name="email"
+            id="email"
+            autoComplete="email"
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel htmlFor="password">Password</FormLabel>
+          <InputGroup size="md">
+            <Input
+              placeholder="Enter a password."
+              color="blue"
+              name="password"
+              id="pass"
+              autoComplete="new-password"
+              value={user.pass}
+              type={show ? "text" : "password"}
+              onChange={(e) => setUser({ ...user, pass: e.target.value })}
+            />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm" onClick={handleClick}>
+                {show ? <ViewOffIcon /> : <ViewIcon />}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </FormControl>
+        <Button type="submit" colorScheme="blue">
           Register
         </Button>
-      </Stack>
-    </div>
+      </Form>
+    </Container>
   );
 }
