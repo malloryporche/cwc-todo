04e5980ae2d039c4a8af35cbce8a854c754d00f4
malloryporche <malloryburke@gmail.com>
2024-02-22 13:16:34 -0500

    Feat(user): Create password RegEx, disabled

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -2,6 +2,8 @@ import { IsEmail, IsString, IsBoolean, IsNotEmpty } from 'class-validator';
 import * as sanitizeHtml from 'sanitize-html';
 import { Transform } from 'class-transformer';
 
+// const passwordRegEx =
+//   /^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,20}$/;
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
