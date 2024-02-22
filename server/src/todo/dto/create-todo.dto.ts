810746740d104fd2a8f4638bbb125530750f3049
malloryporche <malloryburke@gmail.com>
2024-02-22 13:14:30 -0500

    feat(server): add DTO for create-todos

--- server/src/todo/dto/create-todo.dto.ts
+++ server/src/todo/dto/create-todo.dto.ts
@@ -1,1 +1,36 @@
 export class CreateTodoDto {}
+import {
+  IsEmail,
+  IsString,
+  IsBoolean,
+  IsNotEmpty,
+  isDate,
+  IsDate,
+} from 'class-validator';
+import * as sanitizeHtml from 'sanitize-html';
+import { Transform } from 'class-transformer';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @Transform((params) => sanitizeHtml(params.value))
+  title: string;
+
+  @IsString()
+  @Transform((params) => sanitizeHtml(params.value))
+  description: string;
+
+  @IsString()
+  @Transform((params) => sanitizeHtml(params.value))
+  tag: string;
+
+  @IsDate()
+  dueDate: Date;
+
+  @IsBoolean()
+  completed: string;
+
+  @IsString()
+  @IsNotEmpty()
+  projectId: number;
+}
