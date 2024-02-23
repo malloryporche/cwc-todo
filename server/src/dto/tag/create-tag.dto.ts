5446b277ed1894bbe8afac4afc7d558e0e13292a
malloryporche <malloryburke@gmail.com>
2024-02-23 17:46:10 -0500

    Feat(tags): Added tag DTOs

--- server/src/dto/tag/create-tag.dto.ts
+++ server/src/dto/tag/create-tag.dto.ts
@@ -1,1 +1,10 @@
-export class CreateTagDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+import * as sanitizeHtml from 'sanitize-html';
+import { Transform } from 'class-transformer';
+
+export class CreateTagDto {
+  @IsString()
+  @IsNotEmpty()
+  @Transform((params) => sanitizeHtml(params.value))
+  name: string;
+}
