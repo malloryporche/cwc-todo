fe79b5c19d759cc313c841ab6ec109395931df31
malloryporche <malloryburke@gmail.com>
2024-02-28 23:31:31 -0500

    Fix(project): Update project count and progress

--- client/src/Components/Projects/ProjectCard.tsx
+++ client/src/Components/Projects/ProjectCard.tsx
@@ -6,21 +6,52 @@ import {
   Text,
   CircularProgress,
   CircularProgressLabel,
+  LinkOverlay,
+  LinkBox,
 } from "@chakra-ui/react";
 
-export default function ProjectCard() {
+export default function ProjectCard({ project }: any) {
+  const completedTasks = project.todos.filter((task: any) => {
+    return task.status === "completed";
+  }).length;
+  const totalTasks = project.todos.length;
+  const progress = Math.round((completedTasks / totalTasks) * 100);
+
   return (
-    <Flex my="3">
-      <Box flex="1">
-        <Heading size="sm">Sam's Project</Heading>
-        <Heading size="md">Create two add banners</Heading>
-        <Text>2 hours</Text>
-      </Box>
-      <Box>
-        <CircularProgress value={59} size="100px" thickness="4px">
-          <CircularProgressLabel>59%</CircularProgressLabel>
-        </CircularProgress>
-      </Box>
-    </Flex>
+    <LinkBox
+      padding={2}
+      borderWidth="1px"
+      borderRadius="xl"
+      my={4}
+      boxShadow={"md"}
+    >
+      <Flex my="3" alignItems={"center"}>
+        <Box flex="1">
+          <Heading size="sm">
+            <LinkOverlay href={`/projects/${project.id}`}>
+              {project.title}
+            </LinkOverlay>
+          </Heading>
+          <Heading size="md">
+            {project.todos.length > 0
+              ? project.todos[0].title
+              : project.description}
+          </Heading>
+          {/* <Text>2 hours</Text> */}
+          <Text>{project.todos.length} tasks</Text>
+        </Box>
+        <Box>
+          <CircularProgress
+            value={progress ? progress : 0}
+            size="100px"
+            thickness="4px"
+          >
+            <CircularProgressLabel>
+              {progress ? `${progress}%` : "0%"}
+            </CircularProgressLabel>
+          </CircularProgress>
+        </Box>
+      </Flex>
+    </LinkBox>
   );
 }
